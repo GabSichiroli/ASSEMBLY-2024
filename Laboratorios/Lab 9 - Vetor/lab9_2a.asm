98e665d9fdc6037f6dd0aeb8d44ba2a34a7d7851
Sichiroli <sichiroli07@gmail.com>
2024-10-23 21:33:41 -0300

    Commit ASM

--- Laboratorios/Lab 9 - Vetor/lab9_2a.asm
+++ Laboratorios/Lab 9 - Vetor/lab9_2a.asm
@@ -8,24 +8,26 @@ MAIN PROC
             MOV  AX, @DATA
             MOV  DS,AX
 
-            CALL VOLTA
-            
-            MOV  CX,6           ;Contador de vezes (Equivalente o número de elementos)
-            LEA  SI, VETOR      ; SI aponta para o primeiro elemento de VETOR. Podemos usar DI
+            CALL VOLTA          ;Leva até a subrotina "VOLTA" para impressão do VETOR
+
       ;Termina o programa
             MOV  AH,4CH
             INT  21H
 MAIN ENDP
 VOLTA PROC
-
-      VOLT:
+      ;Imprime o vetor inserido em data
+      ;Entrada da varivel vetor
+      ;Não há saida
+            MOV  CX,6           ;Contador de vezes (Equivalente o número de elementos)
+            LEA  SI, VETOR      ; SI aponta para o primeiro elemento de VETOR. Podemos usar DI
+      VOLT: 
             MOV  DL, [SI]       ;Insere o valor do elemento de determinada posição do vetor em DL, a posição do vetor é determinado pelo valor de SI, OU SEJA VETOR[0],VETOR[1]...VETOR[6]
             INC  SI             ;Incrementa SI (SI++) para mudar a posição (SI anterior +1)
             ADD  DL, 30H        ;Transforma em caracter
             MOV  AH, 02         ;Imprime
             INT  21H
 
-            LOOP VOLT:          ;Faz o laco até CX = 0
-            RET
+            LOOP VOLT           ;Faz o laco até CX = 0
+            RET                 ;Retorna a função principal
 VOLTA ENDP
 END MAIN
--- Laboratorios/Lab 9 - Vetor/lab9_2b.asm
+++ Laboratorios/Lab 9 - Vetor/lab9_2b.asm
@@ -1,29 +1,35 @@
 TITLE PROGRAMA EXEMPLO PARA MANIPULAÇÃO DE VETORES USANDO SI ou DI
 .MODEL SMALL
 .DATA
-    VETOR DB 1, 1, 1, 2, 2, 2
+      VETOR DB 1, 1, 1, 2, 2, 2
 .CODE
 MAIN PROC
-    ;Acesso ao DATA
-          MOV  AX, @DATA
-          MOV  DS,AX
+      ;Acesso ao DATA
+            MOV  AX, @DATA
+            MOV  DS,AX
+            
+            CALL VOLTA              ;Leva até a subrotina "VOLTA" para impressão do VETOR
 
-          MOV  CX,6             ;Contador de vezes (Equivalente o número de elementos)
-          XOR  DI, DI           ;Inicializando DI (Pode usar SI)
-
-    ;IMPRIMI O VETOR
-    VOLTA:
-
-          MOV  DL, VETOR[DI]    ;Insere o valor do elemento de determinada posição do vetor em DL, a posição do vetor é determinado pelo valor de DI, OU SEJA VETOR[0],VETOR[1]...VETOR[6]
-          INC  DI               ;Incrementa DI (DI++) para mudar a posição (DI anterior +1)
-          ADD  DL, 30H          ;Transforma em caracter
-          MOV  AH, 02           ;Imprime
-          INT  21H
-
-          LOOP VOLTA            ;Faz o laco até CX = 0
-
-    ;Termina o programa
-          MOV  AH,4CH
-          INT  21H
+      ;Termina o programa
+            MOV  AH,4CH
+            INT  21H
 MAIN ENDP
+      ;IMPRIMI O VETOR
+VOLTA PROC
+      ;Imprime o vetor inserido em data
+      ;Entrada da varivel vetor
+      ;Não há saida
+            MOV  CX,6               ;Contador de vezes (Equivalente o número de elementos)
+            XOR  DI, DI             ;Inicializando DI (Pode usar SI)
+
+      VOLTA:
+            MOV  DL, VETOR[DI]      ;Insere o valor do elemento de determinada posição do vetor em DL, a posição do vetor é determinado pelo valor de DI, OU SEJA VETOR[0],VETOR[1]...VETOR[6]
+            INC  DI                 ;Incrementa DI (DI++) para mudar a posição (DI anterior +1)
+            ADD  DL, 30H            ;Transforma em caracter
+            MOV  AH, 02             ;Imprime
+            INT  21H
+
+            LOOP VOLTA              ;Faz o laco até CX = 0
+            RET                     ;Retorna a função principal
+VOLTA ENDP
 END MAIN
